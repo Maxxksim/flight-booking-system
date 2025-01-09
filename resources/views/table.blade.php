6d73576513ceb6513f1dc3052fc53354e682a0c8
Maxxksim <makximchagin@gmail.com>
2025-01-09 12:52:20 +0200

    Delete views table

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -2,8 +2,8 @@
 <x-app-layout>
     <div class="container mx-auto py-8">
         @if(Auth::user()->name == 'admin')
-            <h2 class="text-xl font-bold text-gray-800 dark:text-gray-200 mt-8 mb-4">Flights</h2>
-            <x-table :headers="$headers" :rows="$rows"/>
+            <h2 class="text-xl font-bold text-gray-800 dark:text-gray-200 mb-4">{{$table}}</h2>
+            <x-table :headers="$headers" :rows="$rows" :table="$table"/>
         @else
             <p class="text-white">You do not have permission to view this page.</p>
         @endif
